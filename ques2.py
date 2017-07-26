09b08a56b759ae5efb2e1dccd68822ad3ead05db
Izzy Kayu <isabel.kayu.metzger@gmail.com>
2017-07-26 02:28:41 -0400

    update on q2
    Please grade q2 initial commit, I just wanted to add my changes, especially for class discussion

--- ques2.py
+++ ques2.py
@@ -11,8 +11,6 @@ p_z_dict = {'ACACIMED': 2, 'acacimed': 2, 'ELEMYRATNE': 1, 'elemyratne': 1, 'wap
             'ZEITTSIEG': 2, 'desfbirc': 1, 'DESFBIRC': 1, 'ALTAATSIV': 3, 'altaatsiv': 3, 'MEINWOHC': 2, 'meinwohc': 2}
 # p_z_dict for protein: charge
 list_p_seq = list(p_z_dict.keys())  # list of protein seq for this assignment
-print("The list of sequences to check if valid are: ")
-print(list_p_seq)
 temp_mass_list = []  # open data list for amino acid masses in input seq
 
 
@@ -23,20 +21,18 @@ def protein_m_z(seq):  # defining protein mass to charge function; variable as s
             m = mass_dict[aa]  # m is for mass of aa in pm
             temp_mass_list.append(m)  # creating temporary list of masses of aa entered
             ch = p_z_dict[seq]  # charge = p_z_dict[pro for protein seq for assignment]
-
-    for num in temp_mass_list:  # creating function
-        # initialize
-        sum_p = sum_p + num
-        mow = 18.010564684  # mass of water (mow)
-        fin_mass = mow + sum_p  # fin_mass as final mass of input AA seq
-        m_z = fin_mass / ch
-    print('Mass (m):\t', '{:.2f}'.format(sum_p))  # formatted to two decimal places
-    print('m/z:\t', '{:.2f}'.format(m_z), '\n')
+            for num in temp_mass_list:  # creating function
+                sum_p = sum_p + num
+                mow = 18.010564684  # mass of water (mow)
+                fin_mass = mow + sum_p  # fin_mass as final mass of input AA seq
+                m_z = fin_mass / ch
+                print('Mass (m):\t', '{:.2f}'.format(sum_p))  # formatted to two decimal places
+                print('m/z:\t', '{:.2f}'.format(m_z), '\n')
 
 
 def check(seq):
     c = 0
-    for letter in seq:
+    for letter in list(seq):
         if letter in list_aa and seq in list_p_seq:
             c = c + 1
     return c
@@ -49,11 +45,10 @@ list_of_protein_dicts = [{'seq': 'ACACIMED', 'ch': 2},
                          {'seq': 'altaatsiv', 'ch': 3},
                          {'seq': 'MEINWOHC', 'ch': 2}]
 
-for element in list_p_seq:
-    for letters in element:
-        if letters in list_aa:
-            print(element)
-            protein_m_z(element)
-        elif letters not in list_aa:
-            print('Invalid')
+for aa_seq in list_p_seq:
+    x = check(aa_seq)
+    if x == len(aa_seq):
+        protein_m_z(aa_seq)
+    elif x < len(aa_seq):
+            print(aa_seq, 'is an invalid protein sequence.\n')
 
